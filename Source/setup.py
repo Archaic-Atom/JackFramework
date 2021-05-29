4ae2ac0bd5f02e6ecb9ad054f4dc056a4c4eef94
JackRao <raoxi36@foxmail.com>
2021-05-29 10:18:54 +0800

    v 0.1.0

--- Source/setup.py
+++ Source/setup.py
@@ -3,11 +3,8 @@ import os
 from setuptools import find_packages, setup
 
 
-def get_all_pkg(source_folder: str, lib_name: str) -> list:
-    all_sub_pkg = [pkg for subdir in os.listdir(source_folder)
-                   if os.path.isdir(os.path.join(source_folder, subdir))
-                   for pkg in find_packages(os.path.join(source_folder, subdir))
-                   ]
+def get_all_pkg(lib_name: str) -> list:
+    all_sub_pkg = find_packages('JackFramework')
     all_pkg = [lib_name + '.' + i for i in all_sub_pkg]
     all_pkg.insert(0, lib_name)
     return all_pkg
@@ -22,13 +19,12 @@ def install_lib(lib_name: str, all_pkg: list,
 
 
 def main():
-    source_folder = '../Source/'
     lib_name = 'JackFramework'
     version = '0.1.0'
     description = 'The deep learning tranining framework based on pytorch.'
     author = 'Jack Rao'
     lic = 'MIT'
-    all_pkg = get_all_pkg(source_folder, lib_name)
+    all_pkg = get_all_pkg(lib_name)
     install_lib(lib_name, all_pkg, version, description, author, lic)
 
 
