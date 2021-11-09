444244aeb7190e920c236717960d664dab3540e4
JackRao <raoxi36@foxmail.com>
2021-11-09 20:17:05 +0800

    remove dataset reader, it will appear in sub project instead of framework

--- Source/JackFramework/DatasetReader/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-# Datasets
-from JackFramework.DatasetReader.stereo_dataset import StereoDataset
--- Source/JackFramework/ImgHandler/__init__.py
+++ Source/JackFramework/ImgHandler/__init__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from JackFramework.ImgHandler.img_handler import ImgHandler 
 from JackFramework.ImgHandler.data_augmentation import DataAugmentation 
 from JackFramework.ImgHandler.img_io import ImgIO 
